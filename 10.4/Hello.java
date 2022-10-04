f33da655db9e89196acb7822a7b46fc5a859aaae
kokorosawa <90631564+kokorosawa@users.noreply.github.com>
2022-10-04 19:52:00 +0800

    Update Hello.java

--- 10.4/Hello.java
+++ 10.4/Hello.java
@@ -1,8 +1,16 @@
-import java.util.Locale;
+/**
+ * This is Hello class.
+ */
 
 public class Hello {
-    public static void main(String[] args) {
-        String name  = "andy";
-        System.out.println("\"Hello " + name.toUpperCase(Locale.ROOT) + "!\"");
-    }
+  /**
+   * This is method main.
+   *
+   *  @param args input value
+   */
+  public static void main(String[] args) {
+    String name  = "andy";
+    System.out.println("\"Hello " + name.toUpperCase() + "!\"");
+    // 印出Hello ANDY!
+  }
 }
