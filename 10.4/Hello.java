67fcb13fb755f6789cf0c64d48953afb22213c87
kokorosawa <90631564+kokorosawa@users.noreply.github.com>
2022-10-04 19:46:03 +0800

    Create Hello.java

---
+++ 10.4/Hello.java
@@ -0,0 +1,8 @@
+import java.util.Locale;
+
+public class Hello {
+    public static void main(String[] args) {
+        String name  = "andy";
+        System.out.println("\"Hello " + name.toUpperCase(Locale.ROOT) + "!\"");
+    }
+}
